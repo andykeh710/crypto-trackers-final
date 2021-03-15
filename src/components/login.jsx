bfba0e6f7deecc8dccc96e01babc613e67ff92d3
andyk <andy.keh@gmail.com>
2021-03-14 20:48:00 -0500

    custom login buttons

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
-
+import { Link } from "react-router-dom";
 import { refreshTokenSetup } from '../utils/refreshToken';
-
+import axios from 'axios';
 
 const clientId = '849863262005-d015trj6hp4piohkfmal41u16n8a3m43.apps.googleusercontent.com';
 
@@ -11,9 +11,15 @@ function Login() {
     const onSuccess = (res) => {
         console.log('[Login Success] current user: ', res.profileObj );
         
-    refreshTokenSetup(res); // handles new token ids 
+        // axios({
+        //     method: "POST",
+        //     url: "http://localhost:8000/api/googlelogin",
+        //     data: {tokenId: response.tokenId} 
+        // }).then(response => {
+        //     console.log(response)
+        // })
+        // refreshTokenSetup(res); // handles new token ids 
     }
-
 
     const onFailure = (res) => {
         console.log('Login failed res: ', res);
@@ -23,13 +29,14 @@ function Login() {
         <div> 
             <GoogleLogin
             clientId={clientId}
-            buttonText="Login"
+            render={renderProps => (
+                <Link className="text-center text-warning mt-3 bm-4" onClick={renderProps.onClick} disabled={renderProps.disabled}>login</Link>
+            )}
             onSuccess={onSuccess}
             onFailure={onFailure}
             cookiePolicy={'single_host_origin'}
-            style={{ marginTop: '100pc' }}
             isSignedIn={true}
-            />
+            />,
         </div>
     )
 }
